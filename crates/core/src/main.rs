9eabc8ccd6656dfd1fc0b0e762af06106056ecc0
VINXIS <oykxf2@gmail.com>
2026-08-26 07:46:42 -0600

    protocol lib crate

--- crates/core/src/main.rs
+++ crates/core/src/main.rs
@@ -10,6 +10,7 @@ mod sender;
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
+    println!("{}", protocol::add(1, 2));
     let mut args = env::args().skip(1);
     let role = args.next().unwrap_or_default();
 

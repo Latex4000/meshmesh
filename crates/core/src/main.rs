c61f47c274835591192d5f0b6da1dbe4e61e5188
VINXIS <oykxf2@gmail.com>
2026-08-27 10:11:48 -0600

    implement echo protocol tutorial with prev ticketing fucntionality

--- crates/core/src/main.rs
+++ crates/core/src/main.rs
@@ -1,11 +1,6 @@
 use anyhow::Result;
+use protocol::{acceptor::accept, connector::connect};
 use std::env;
-
-use crate::receiver::run_receiver;
-use crate::sender::run_sender;
-
-mod receiver;
-mod sender;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -15,7 +10,7 @@ async fn main() -> Result<()> {
     let role = args.next().unwrap_or_default();
 
     match role.as_str() {
-        "sender" => run_sender(&mut args).await,
-        _ => run_receiver().await,
+        "sender" => connect(&mut args).await,
+        _ => accept().await,
     }
 }
--- crates/protocol/src/lib.rs
+++ crates/protocol/src/lib.rs
@@ -1,3 +1,44 @@
+use iroh::{endpoint::Connection, protocol::AcceptError};
+
+pub const ALPN: &[u8] = b"meshmesh/1";
+
+pub mod acceptor;
+pub mod connector;
+
+#[derive(Debug, Clone)]
+pub struct Protocol;
+
+impl iroh::protocol::ProtocolHandler for Protocol {
+    /// The `accept` method is called for each incoming connection for our ALPN.
+    ///
+    /// The returned future runs on a newly spawned tokio task, so it can run as long as
+    /// the connection lasts without blocking other connections.
+    async fn accept(&self, connection: Connection) -> Result<(), AcceptError> {
+        // We can get the remote's endpoint id from the connection.
+        let endpoint_id = connection.remote_id();
+        println!("accepted connection from {endpoint_id}");
+
+        // Our protocol is a simple request-response protocol, so we expect the
+        // connecting peer to open a single bi-directional stream.
+        let (mut send, mut recv) = connection.accept_bi().await?;
+
+        // Echo any bytes received back directly.
+        // This will keep copying until the sender signals the end of data on the stream.
+        let bytes_sent = tokio::io::copy(&mut recv, &mut send).await?;
+        println!("Copied over {bytes_sent} byte(s)");
+
+        // By calling `finish` on the send stream we signal that we will not send anything
+        // further, which makes the receive stream on the other end terminate.
+        send.finish()?;
+
+        // Wait until the remote closes the connection, which it does once it
+        // received the response.
+        connection.closed().await;
+
+        Ok(())
+    }
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }

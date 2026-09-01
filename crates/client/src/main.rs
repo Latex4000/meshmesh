a1dc93f909df7cd9e334bc88df4de76bd1911765
riesha <seleneia@outlook.jp>
2026-09-01 20:07:51 +0200

    add clearpeers command for debugging

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -50,7 +50,7 @@ async fn command_line() -> anyhow::Result<()> {
 
                 match cmd {
                     "help" => println!(
-                        "cmds: help, state, peers, discover <ep>, join <roomid>, ping <peerid>, pingall, exit"
+                        "cmds: help, state, peers, discover <ep>, join <roomid>, ping <peerid>, pingall, clearpeers, exit"
                     ),
                     "discover" => {
                         if let Err(e) = Peer::discover(&args.join(" ")).await {
@@ -79,6 +79,12 @@ async fn command_line() -> anyhow::Result<()> {
                                 Ok(id) => println!("You have not discovered peer ID {id}"),
                                 Err(e) => println!("Could not connect to ID.\n{e}"),
                             }
+                        }
+                    }
+                    "clearpeers" => {
+                        let mutex = CLIENT_CTX.get().ok_or(anyhow!("couldnt get mutex"))?;
+                        if let Ok(mut ctx) = mutex.lock() {
+                            ctx.peers.clear();
                         }
                     }
                     _ => match client_window {

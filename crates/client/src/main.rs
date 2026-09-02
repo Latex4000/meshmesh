4bbe0190b26918fde94c78939c326ae4e779c868
VINXIS <oykxf2@gmail.com>
2026-09-02 10:35:52 -0600

    actually quit

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -96,7 +96,8 @@ async fn command_line() -> Result<(), Error> {
                 }
             }
             Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
-                eprintln!("quit");
+                println!("quit");
+                return Ok(());
             }
             Err(err) => {
                 println!("Error: {err}");

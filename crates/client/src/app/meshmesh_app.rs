aeb35fee8c78fb90535954c38542f0595626394f
Ashley <ccccccmail@gmail.com>
2026-09-04 21:47:57 +0100

    Add basic initial app stylesheet

--- crates/client/src/app/meshmesh_app.rs
+++ crates/client/src/app/meshmesh_app.rs
@@ -1,10 +1,14 @@
 #[cfg(feature = "gui")]
 use dioxus_native::prelude::*;
 
+static APP_CSS: Asset = asset!("../assets/style.css");
+
 #[cfg(feature = "gui")]
 #[component]
 pub fn MeshmeshApp() -> Element {
     rsx! {
+        document::Stylesheet { href: APP_CSS }
+
         div {
             h1 { "welcome to my app" }
         }
